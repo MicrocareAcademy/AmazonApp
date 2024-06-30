39fce96c3abe7fe3a2a761f877f29b580c8a6531
mansooralikhanbhatti <mansooralikhan9360@gmail.com>
2024-06-30 17:01:04 +0530

    Users displayed in the dashboard page

---
+++ Areas/Account/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AmazonApp.Areas.Account.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public int UserId { get; set; }
+    }
+}
--- Areas/Home/Controllers/HomeController.cs
+++ Areas/Home/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AmazonApp.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AmazonApp.Areas.Home.Controllers
 {
     [Area("Home")]
     public class HomeController : Controller
     {
+        AmazonDBContext dBContext = new AmazonDBContext();
         public IActionResult Dashboard()
         {
-            return View();
+            var users = dBContext.Users.ToList();
+            return View("Dashboard");
         }
     }
 }
