0296a92ed8ac8ca2c0a3150058c91457c5e6677f
HammadJasir <hammad.jasir.1@gmail.com>
2024-06-30 15:31:00 +0400

    Categories CRUD added

--- Entities/AmazonDBContext.cs
+++ Entities/AmazonDBContext.cs
@@ -15,6 +15,8 @@ public partial class AmazonDBContext : DbContext
     {
     }
 
+    public virtual DbSet<Category> Categories { get; set; }
+
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -23,6 +25,22 @@ public partial class AmazonDBContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__category__19093A2BBE70C3D7");
+
+            entity.ToTable("category");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryDescription).HasColumnType("text");
+            entity.Property(e => e.CategoryName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.CategoryStatus)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.UserId).HasName("PK__Users__B9BE370FC1EA914A");
